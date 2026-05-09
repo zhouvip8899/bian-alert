2894338be94a4e9254a0dc390d2e784eeaa70491
zhouvip8899 <zhouvip8899@users.noreply.github.com>
2026-05-09 20:21:48 +0800

    pro-kline: WS多代理轮询+自动加载+symbol校验 v3.54

---
+++ server.js
@@ -0,0 +1,36 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8766;
+const ROOT = __dirname;
+
+const mime = {
+  '.html': 'text/html; charset=utf-8',
+  '.js':   'application/javascript; charset=utf-8',
+  '.css':  'text/css; charset=utf-8',
+  '.json': 'application/json',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  let urlPath = req.url.split('?')[0];
+  if (urlPath === '/' || urlPath === '') urlPath = '/index.html';
+  const filePath = path.join(ROOT, urlPath);
+  const ext = path.extname(filePath).toLowerCase();
+  const contentType = mime[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404); res.end('Not found: ' + urlPath);
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+}).listen(PORT, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:' + PORT);
+});
